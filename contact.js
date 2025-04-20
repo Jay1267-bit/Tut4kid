983294f0389a6b50f2dcc866806988febe1f4587
Jayanth G <Tut4kid@gmail.com>
2025-04-20 10:17:18 -0500

    first commit

---
+++ contact.js
@@ -0,0 +1,8 @@
+<><form id="template_yvbhh2b">
+</form><script>
+        document.getElementById("template_yvbhh2b").addEventListener("submit", function(e) {e.preventDefault()};
+        emailjs.sendForm("service_77eth8j", "template_yvbhh2b", this)
+        .then(() = alert("Message sent!"))
+        .catch(() = alert("Failed to send"));
+        );
+    </script></>
